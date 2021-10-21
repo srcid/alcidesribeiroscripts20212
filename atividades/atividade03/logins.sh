5f0aba4c40add7cc2384ce0ccce160582753f5e4
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2021-10-21 18:26:47 -0300

    atividade03 adicionada

---
+++ atividades/atividade03/logins.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Um comando grep que encontre todoas as linhas com mensagens que não são do sshd
+grep -v 'sshd' auth.log
+
+# Um comando grep que encontre todas as linhas com mensagens que indicam um login de sucesso via sshd cujo nome do usuário começa com a letra j
+egrep 'sshd.+ session opened for user j' auth.log
+# ou ainda
+egrep 'sshd.+ Accepted .+ for j' auth.log
+
+# Um comando grep que encontre todas as vezes que alguém tentou fazer login via root através do sshd
+egrep 'sshd.+ (Disconnected|Accepted).+ root' auth.log
+
+# Um comando grep que encontre todas as vezes que alguém conseguiu fazer login com sucesso nos dias 11 ou 12 de Outubro
+egrep 'Oct (11|12) .+ sshd.+ Accepted' auth.log
