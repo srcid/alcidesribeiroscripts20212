105d6953129dbf72bd33583b3eabb019f0bf7ec6
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2021-11-04 18:44:25 -0300

    alterando expr para bc em atividade05

--- atividades/atividade05/maiorde3.sh
+++ atividades/atividade05/maiorde3.sh
@@ -3,13 +3,13 @@
 [ $# -lt 3 ] || [ $# -gt 3 ] && echo "Forneça três parametros!" && exit 1
 
 for param in $*; do
-  [[ $param =~ ^[[:digit:]]+(\.[[:digit:]]+)?$ ]] || echo "$param não é numero!" || exit 2
+  [[ $param =~ ^-?[[:digit:]]+(\.[[:digit:]]+)?$ ]] || echo "$param não é numero!" || exit 2
 done
 
 max=$1
 
 for i in ${*:2}; do
-  [ $(expr "$i" ">" "$max") == 1 ] && max=$i
+  [ $(echo "$i > $max" | bc) == 1 ] && max=$i
 done
 
 echo $max
