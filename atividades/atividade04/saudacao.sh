46dfe7cc2579b80cc218e71230a4ac05b894abdd
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2021-11-04 18:57:38 -0300

    corrigindo atividade04

--- atividades/atividade04/saudacao.sh
+++ atividades/atividade04/saudacao.sh
@@ -6,4 +6,4 @@ user=$(whoami)
 
 read -r cur_day cur_mouth cur_year <<< $(date +'%d %m %y')
 
-echo -e "Olá, $user!\nHoje é dia $cur_day, do mês $cur_mouth do ano de $cur_year." | tee saudacao.log
+echo -e "Olá, $user!\nHoje é dia $cur_day, do mês $cur_mouth do ano de $cur_year." | tee -a saudacao.log
